fc6bcbe7ef1edd4f452af2604687bd7dd6d1bfb0
heliosalesjr <heliosalesjr@gmail.com>
2024-01-22 16:23:18 -0300

    bug fix

--- src/app/atividades/page.jsx
+++ src/app/atividades/page.jsx
@@ -7,11 +7,9 @@ import AtiBotaoNext from '../components/Atividades/AtiBotaoNext'
 import AtiDiagnostico2 from '../components/Atividades/AtiDiagnostico2'
 import AtiDica from '../components/Atividades/AtiDica'
 import AtivHero from '../components/Atividades/AtivHero'
-import Divider from '../components/Divider'
+
 import AtiProfessores from '../components/Atividades/AtiProfessores'
 import AtiDiag1 from '../components/Atividades/AtiDiag1'
-import AtiDiag1part2 from '../components/Atividades/AtiDiag1-2'
-import TestingScroll from '../components/Atividades/TestingScroll'
 import AtiDiagEstudAcc from '../components/Atividades/AtiDiagEstudAcc'
 import AtiCardsEstud from '../components/Atividades/AtiCardsEstud'
 
